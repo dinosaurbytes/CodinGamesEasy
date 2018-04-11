5b114236b4f73434ea7b780b170f522c3eac441c
Brian Liang <brianliang@Brians-MacBook-Pro.local>
2018-04-11 11:17:06 -0600

    added AsciiArt Solution.java

---
+++ AsciiArt/Solution.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+class Solution {
+    
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int L = in.nextInt();
+        int H = in.nextInt();
+        if (in.hasNextLine()){
+            in.nextLine();
+        }
+        String T = in.nextLine();
+        for (int i = 0; i < H; i++){
+            String ROW = in.nextLine();
+        }
+        
+        // create an array to store alphabet art
+        String [i][j] asciiArt = ROW
+        
+        
+	    System.out.println(T);
+    }
+}
