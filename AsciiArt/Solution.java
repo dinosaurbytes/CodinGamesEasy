c7fa14f818e9f6fefdfe6cdecdca8e24e8324594
Brian Liang <brianliang@Brians-MacBook-Pro.local>
2018-04-11 18:21:27 -0600

    test commit

--- AsciiArt/Solution.java
+++ AsciiArt/Solution.java
@@ -5,21 +5,34 @@ import java.math.*;
 class Solution {
     
     public static void main(String args[]){
-        Scanner in = new Scanner(System.in);
+	
+	Scanner in = new Scanner(System.in);
         int L = in.nextInt();
         int H = in.nextInt();
-        if (in.hasNextLine()){
+	
+
+	if (in.hasNextLine()){
             in.nextLine();
         }
         String T = in.nextLine();
-        for (int i = 0; i < H; i++){
+        for (int i = 0; i < H; i++){    //going down the rows inputting the string
             String ROW = in.nextLine();
-        }
+	    char[] artArray = ROW.toCharArray();
+
+	    for (int j = 0; j < ROW.length; j++)
+		
+		}
+	System.out.println(artArray);
         
         // create an array to store alphabet art
-        String [i][j] asciiArt = ROW
+	/* Test
+	System.out.println(L);
+	System.out.println(H);
+	System.out.println(T);
+	*/
+
+
         
-        
-	    System.out.println(T);
+       
     }
 }
