4e43151977feec77301760aa543d4cc832802f83
Brian Liang <brianliang@Brians-MacBook-Pro.local>
2018-04-12 14:47:04 -0600

    3rd commit attempt

--- HorseRacingDuals/Solution.java
+++ HorseRacingDuals/Solution.java
@@ -1,39 +1,33 @@
 import java.util.*;
 import java.io.*;
 import java.math.*;
-import java.util.Arrays;
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
+
 class Solution {
 
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
-	// Create an array to store horses
-	int[] horses = new int[N];
-	
-
+	// create an array to store values
+	int[] arr = new int[N];
         for (int i = 0; i < N; i++) {
-            int pi = in.nextInt();
-
-	    // store pi in horse array
-	    horses[i] = pi;
-
-	    // Sort array
-	    Collections.sort( horses);
-	    
+            arr[i] = in.nextInt();
         }
-	for (int a = 0; a < horses.length; a++)
-	    System.out.print(horses[a] + " ");
-	System.out.println();
-	
-        // Write an action using System.out.println()
-        // To debug: System.err.println("Debug messages...");
+	//Sort array
+	Arrays.sort(arr);
 
-	//   System.out.println(Arrays.toString(horses));
-       
+	// find smallest difference
+	int smallestDif = 10000;
+	
+	for(int i = 0, j = 1; i < N-1; i++){
+	    if (arr[j]-arr[i] <= smallestDif)
+		smallestDif = arr[j]-arr[i];
+	    j++;
+	}
+	
+      
+
+        System.out.println(Arrays.toString(arr));
+	System.out.println(smallestDif);
     }
 }
