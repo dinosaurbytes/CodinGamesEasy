7d8a2e8ace8e5701dccc6c2a2926df045638d441
Brian Liang <brianliang@Brians-MacBook-Pro.local>
2018-04-16 11:55:04 -0600

    new commit for ascii art

--- AsciiArt/Solution.java
+++ AsciiArt/Solution.java
@@ -10,20 +10,35 @@ class Solution {
         int L = in.nextInt();
         int H = in.nextInt();
 	String[] arr = new String[H];
+	
 	if (in.hasNextLine()) {
             in.nextLine();
         }
-        String T = in.nextLine();
+        String T = in.nextLine(); //inputed phrase Q1 is E
+	// sorting the next lines A,B,C asciiart in an array called arr
         for (int i = 0; i < H; i++) {
             arr[i] = in.nextLine();
         }
 
+	// change string T into char array
+	char[] ch = T.toCharArray();
+	System.out.println(ch[0]);
+	int ascii = (int) ch[0] - (int) 'A';
+	System.out.println(ascii);
+	
+	//printing ascii art
+	for (int i = 0; i < H; i++)
+	    System.out.println(arr[i].substring(ascii, ascii + L));
+
+
         // Write an action using System.out.println()
         // To debug: System.err.println("Debug messages...");
-
-        System.out.println(arr[0]);
+	// arr[0].substring(2,4)
+	/*
+        System.out.println(arr[0].substring(2,4));
 	System.out.println(arr[1]);
 	System.out.println(arr[2]);
+	*/
     }
 }
 
