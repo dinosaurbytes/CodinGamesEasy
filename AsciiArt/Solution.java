4f0ef067a9fbd4562809e545cec30d877226d857
Brian Liang <brianliang@Brians-MacBook-Pro.local>
2018-04-18 13:06:00 -0600

    final solution

--- AsciiArt/Solution.java
+++ AsciiArt/Solution.java
@@ -4,42 +4,44 @@ import java.math.*;
 
 class Solution {
     
+    static final char FIRST_CHAR = 'A';
+    static final char LAST_CHAR = 'Z';
+    static final char QUESTION_MARK = LAST_CHAR + 1;
+    
+    public static void printChar(int l, char c, String[] rep, int w) {
+        int begin = (c - FIRST_CHAR) * w;
+        int end = begin + w;
+        System.out.print(rep[l].substring(begin, end));
+    }
+        
+    
     public static void main(String args[]){
-	
-	Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
         int L = in.nextInt();
         int H = in.nextInt();
-	String[] arr = new String[H];
 	
 	if (in.hasNextLine()) {
             in.nextLine();
         }
-        String T = in.nextLine(); //inputed phrase Q1 is E
-	// sorting the next lines A,B,C asciiart in an array called arr
+        
+        // Text to display
+        String T = in.nextLine().toUpperCase(); 
+        
+        //Ascii Art
+        String[] arr = new String[H];
         for (int i = 0; i < H; i++) {
             arr[i] = in.nextLine();
         }
 
-	// change string T into char array
-	char[] ch = T.toCharArray();
-	System.out.println(ch[0]);
-	int ascii = (int) ch[0] - (int) 'A';
-	System.out.println(ascii);
-	
-	//printing ascii art
-	for (int i = 0; i < H; i++)
-	    System.out.println(arr[i].substring(ascii, ascii + L));
-
-
-        // Write an action using System.out.println()
-        // To debug: System.err.println("Debug messages...");
-	// arr[0].substring(2,4)
-	/*
-        System.out.println(arr[0].substring(2,4));
-	System.out.println(arr[1]);
-	System.out.println(arr[2]);
-	*/
+        // print results
+        for (int l=0; l < H; l++){
+            for (char c : T.toCharArray())
+                if (FIRST_CHAR <= c && LAST_CHAR >= c)
+		    printChar(l,c,arr,L);
+		else 
+		    printChar(l,QUESTION_MARK,arr,L);
+	    System.out.println();
+	}
     }
+    
 }
-
-              
